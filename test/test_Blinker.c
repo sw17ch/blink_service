827574a94422dd4c558ab786c1fe5695496c48b1
John Van Enk <vanenkj@gmail.com>
2012-06-28 10:17:55 -0400

    Making test name better.

--- test/test_Blinker.c
+++ test/test_Blinker.c
@@ -21,7 +21,7 @@ void test_Blinker_Init(void)
   Blinker_Init(&blinker);
 }
 
-void test_Blinker_should_toggle_light_if_alarm_is_expired(void)
+void test_Blinker_Service_should_toggle_light_if_alarm_is_expired(void)
 {
   Alarm_IsExpired_ExpectAndReturn(&blinker.alarm, true);
   Blinker_LED_Toggle_Expect(&blinker.led);
@@ -30,7 +30,7 @@ void test_Blinker_should_toggle_light_if_alarm_is_expired(void)
   Blinker_Service(&blinker);
 }
 
-void test_Blinker_should_not_toggle_if_alarm_has_not_expired(void)
+void test_Blinker_Service_should_not_toggle_if_alarm_has_not_expired(void)
 {
   Alarm_IsExpired_ExpectAndReturn(&blinker.alarm, false);
 
