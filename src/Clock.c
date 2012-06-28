fe90c3cbf40de533a5b2497cd2cdceb9ac7588ca
John Van Enk <vanenkj@gmail.com>
2012-06-27 20:10:36 -0400

    Implemented clock.

--- src/Clock.c
+++ src/Clock.c
@@ -1,18 +1,23 @@
 #include "Clock.h"
 
+static Clock_Val_t clock = 0;
+
 void Clock_Tick(void)
 {
+  clock++;
 }
 
 void Clock_Reset(void)
 {
+  clock = 0;
 }
 
 Clock_Val_t Clock_Get(void)
 {
-  return 0;
+  return clock;
 }
 
 void Clock_Set(Clock_Val_t time_to_set)
 {
+  clock = time_to_set;
 }
--- test/test_Clock.c
+++ test/test_Clock.c
@@ -4,8 +4,36 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-void test_implement_me(void)
+void test_Clock_Tick(void)
 {
-  TEST_IGNORE_MESSAGE("Implement me!");
+  Clock_Set(0);
+  Clock_Tick();
+  TEST_ASSERT_EQUAL(1, Clock_Get());
+  Clock_Tick();
+  TEST_ASSERT_EQUAL(2, Clock_Get());
+  Clock_Set(10);
+  Clock_Tick();
+  TEST_ASSERT_EQUAL(11, Clock_Get());
 }
 
+void test_Clock_Reset(void)
+{
+  Clock_Set(10);
+  Clock_Reset();
+
+  TEST_ASSERT_EQUAL(0, Clock_Get());
+}
+
+void test_Clock_Get(void)
+{
+  Clock_Set(12);
+  TEST_ASSERT_EQUAL(12, Clock_Get());
+}
+
+void test_Clock_Set(void)
+{
+  Clock_Set(12);
+  TEST_ASSERT_EQUAL(12, Clock_Get());
+  Clock_Set(20);
+  TEST_ASSERT_EQUAL(20, Clock_Get());
+}
