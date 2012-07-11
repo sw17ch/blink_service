140c90e01519b3318e369911f983206099c39124
John Van Enk <vanenkj@gmail.com>
2012-07-10 20:07:36 -0400

    Finished implementation of ring buffer.

--- include/RingBuffer.h
+++ include/RingBuffer.h
@@ -6,14 +6,16 @@
 #include <stdbool.h>
 
 typedef struct {
-  uint8_t * backing; // The buffer backing the ring.
-  size_t size;       // The size of the backing buffer.
-  size_t head;       // The head of the ring buffer.
-  size_t tail;       // The last element in the ring buffer.
-  size_t len;        // The number of elements in the ring buffer.
+  uint8_t * backing;   // The buffer backing the ring.
+  size_t backing_size; // The size of the backing buffer.
+  size_t head;         // The head of the ring buffer.
+  size_t tail;         // The last element in the ring buffer.
+  size_t len;          // The number of elements in the ring buffer.
 } RingBuffer_t;
 
 void RingBuffer_Init(RingBuffer_t * rb, uint8_t * backing, size_t size);
 bool RingBuffer_Put(RingBuffer_t * rb, uint8_t val);
+bool RingBuffer_Get(RingBuffer_t * rb, uint8_t * val);
+void RingBuffer_Reset(RingBuffer_t * rb);
 
 #endif // RingBuffer_H
--- test/test_RingBuffer.c
+++ test/test_RingBuffer.c
@@ -28,13 +28,14 @@ void test_RingBuffer_Init_initializes_a_new_ring_buffer(void)
 
   RingBuffer_Init(&rb, backing, BACKING_SIZE);
 
-  TEST_ASSERT_EQUAL(rb.backing, backing);
-  TEST_ASSERT_EQUAL(rb.head, 0);
-  TEST_ASSERT_EQUAL(rb.tail, 0);
-  TEST_ASSERT_EQUAL(rb.len, 0);
+  TEST_ASSERT_EQUAL(backing, rb.backing);
+  TEST_ASSERT_EQUAL(BACKING_SIZE, rb.backing_size);
+  TEST_ASSERT_EQUAL(0, rb.head);
+  TEST_ASSERT_EQUAL(0, rb.tail);
+  TEST_ASSERT_EQUAL(0, rb.len);
 }
 
-void test_RingBuffer_Put_stores_valies(void)
+void test_RingBuffer_Put_stores_values(void)
 {
   TEST_ASSERT_TRUE(RingBuffer_Put(&rb, 'A'));
   TEST_ASSERT_TRUE(RingBuffer_Put(&rb, 'B'));
@@ -42,11 +43,89 @@ void test_RingBuffer_Put_stores_valies(void)
   TEST_ASSERT_EQUAL('B', rb.backing[1]);
 }
 
-void test_RingBuffer_Put_is_false_on_overflow(void)
+void test_RingBuffer_Put_adjusts_len(void)
 {
+  TEST_ASSERT_EQUAL(0, rb.len);
+  RingBuffer_Put(&rb, 'A');
+  TEST_ASSERT_EQUAL(1, rb.len);
+  RingBuffer_Put(&rb, 'B');
+  TEST_ASSERT_EQUAL(2, rb.len);
+}
+
+void test_RingBuffer_Put_is_handles_overflows(void)
+{
+  uint8_t expected_backing[] = {'A','B','C','D', 0};
+
   TEST_ASSERT_TRUE(RingBuffer_Put(&rb, 'A'));
-  TEST_ASSERT_TRUE(RingBuffer_Put(&rb, 'A'));
-  TEST_ASSERT_TRUE(RingBuffer_Put(&rb, 'A'));
-  TEST_ASSERT_TRUE(RingBuffer_Put(&rb, 'A'));
-  TEST_ASSERT_FALSE(RingBuffer_Put(&rb, 'A')); // overflow
+  TEST_ASSERT_TRUE(RingBuffer_Put(&rb, 'B'));
+  TEST_ASSERT_TRUE(RingBuffer_Put(&rb, 'C'));
+  TEST_ASSERT_TRUE(RingBuffer_Put(&rb, 'D'));
+  TEST_ASSERT_FALSE(RingBuffer_Put(&rb, 'E')); // overflow, should be false
+
+  TEST_ASSERT_EQUAL_MEMORY(expected_backing, backing, sizeof(expected_backing));
+
+}
+
+void test_RingBuffer_Get_is_true_when_data_is_valid(void)
+{
+  uint8_t val = 0;
+
+  RingBuffer_Put(&rb, 'A');
+
+  TEST_ASSERT_TRUE(RingBuffer_Get(&rb, &val));
+  TEST_ASSERT_EQUAL('A', val);
+
+  RingBuffer_Put(&rb, 'B');
+
+  TEST_ASSERT_TRUE(RingBuffer_Get(&rb, &val));
+  TEST_ASSERT_EQUAL('B', val);
+}
+
+void test_RingBuffer_Get_is_false_when_data_is_invalid(void)
+{
+  uint8_t val = 0;
+  TEST_ASSERT_FALSE(RingBuffer_Get(&rb, &val));
+
+  RingBuffer_Put(&rb, 'A');
+  TEST_ASSERT_TRUE(RingBuffer_Get(&rb, &val));
+  TEST_ASSERT_FALSE(RingBuffer_Get(&rb, &val));
+}
+
+void test_RingBuffer_Get_can_wrap_around(void)
+{
+  uint8_t val = 0;
+
+  // Fill the buffer.
+  RingBuffer_Put(&rb, 'A');
+  RingBuffer_Put(&rb, 'B');
+  RingBuffer_Put(&rb, 'C');
+  RingBuffer_Put(&rb, 'D');
+
+  // Pull something out.
+  RingBuffer_Get(&rb, &val);
+  TEST_ASSERT_EQUAL('A', val);
+
+  // Put two more things in. Second should fail.
+  RingBuffer_Put(&rb, 'E');
+  TEST_ASSERT_FALSE(RingBuffer_Put(&rb, 'X'));
+
+  // Pull everything out.
+  RingBuffer_Get(&rb, &val);
+  TEST_ASSERT_EQUAL('B', val);
+  RingBuffer_Get(&rb, &val);
+  TEST_ASSERT_EQUAL('C', val);
+  RingBuffer_Get(&rb, &val);
+  TEST_ASSERT_EQUAL('D', val);
+  RingBuffer_Get(&rb, &val);
+  TEST_ASSERT_EQUAL('E', val);
+}
+
+void test_RingBuffer_Reset_puts_the_ring_buffer_at_its_original_state(void)
+{
+  RingBuffer_Put(&rb, 'A');
+  RingBuffer_Reset(&rb);
+
+  TEST_ASSERT_EQUAL(0, rb.len);
+  TEST_ASSERT_EQUAL(0, rb.head);
+  TEST_ASSERT_EQUAL(0, rb.tail);
 }
