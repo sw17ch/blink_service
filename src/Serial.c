b292fe5b13dfc5816d205f9ae203bff11bf655c7
John Van Enk <vanenkj@gmail.com>
2012-08-03 12:41:16 -0400

    Now we have separate buffers for both rx and tx. Oops.

--- src/Serial.c
+++ src/Serial.c
@@ -4,7 +4,8 @@
 
 void Serial_Init(Serial_t * serial)
 {
-  RingBuffer_Init(&serial->rb, serial->rb_backing, sizeof(serial->rb_backing));
+  RingBuffer_Init(&serial->tx_rb, serial->tx_rb_backing, sizeof(serial->tx_rb_backing));
+  RingBuffer_Init(&serial->rx_rb, serial->rx_rb_backing, sizeof(serial->rx_rb_backing));
   UART_Init(Serial_Callbacks_RX, (void *)serial,
             Serial_Callbacks_TX, (void *)serial);
 }
@@ -16,7 +17,7 @@ size_t Serial_Put(Serial_t * serial, uint8_t * bytes, size_t len)
 
   while(0 < remaining)
   {
-    if(RingBuffer_Put(&serial->rb, bytes[i]))
+    if(RingBuffer_Put(&serial->tx_rb, bytes[i]))
     {
       i++;
       remaining--;
@@ -42,7 +43,7 @@ size_t Serial_Get(Serial_t * serial, uint8_t * bytes, size_t len)
 
   for (gotten = 0; gotten < len; gotten++)
   {
-    if(RingBuffer_Get(&serial->rb, &byte))
+    if(RingBuffer_Get(&serial->rx_rb, &byte))
     {
       bytes[gotten] = byte;
     }
@@ -58,11 +59,11 @@ size_t Serial_Get(Serial_t * serial, uint8_t * bytes, size_t len)
 void Serial_Callbacks_RX(uint8_t byte, void * param)
 {
   Serial_t * serial = (Serial_t *)param;
-  RingBuffer_Put(&serial->rb, byte); // currently not checking for overflow
+  RingBuffer_Put(&serial->rx_rb, byte); // currently not checking for overflow
 }
 
 bool Serial_Callbacks_TX(uint8_t * byte, void * param)
 {
   Serial_t * serial = (Serial_t *)param;
-  return RingBuffer_Get(&serial->rb, byte);
+  return RingBuffer_Get(&serial->tx_rb, byte);
 }
